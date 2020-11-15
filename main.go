8913ae235062b3d97630facce1dd65aacdda5f9a
sammyonyekwere <saamzinc@gmail.com>
2020-11-15 22:36:09 +0100

    created functions

--- main.go
+++ main.go
@@ -8,6 +8,27 @@ import (
 )
 
 func main() {
+
+	// creating new report PDF
+	pdf := newReport()
+
+	// pdf table header
+	pdf = header(pdf, []string{"1st column", "2nd", "3rd", "4th", "5th", "6th"})
+
+	// pdf table content
+	pdf = table(pdf)
+
+	if pdf.Err() {
+		log.Fatalf("failed! %s", pdf.Error())
+	}
+
+	err := pdf.OutputFileAndClose("report-example.pdf")
+	if err != nil {
+		log.Fatalf("error saving pdf file: %s", err)
+	}
+}
+
+func newReport() *gofpdf.Fpdf {
 	pdf := gofpdf.New("L", "mm", "A4", "")
 	pdf.AddPage()
 
@@ -19,17 +40,24 @@ func main() {
 	pdf.Cell(40, 10, time.Now().Format("Mon Jan 2, 2006"))
 	pdf.Ln(20)
 
+	return pdf
+
+}
+
+func header(pdf *gofpdf.Fpdf, headerText []string) *gofpdf.Fpdf {
 	pdf.SetFont("Times", "", 20)
 	pdf.SetFillColor(240, 240, 240)
 
-	pdf.CellFormat(40, 10, "12", "1", 0, "", true, 0, "")
-	pdf.CellFormat(40, 10, "22", "1", 0, "", true, 0, "")
-	pdf.CellFormat(40, 10, "25", "1", 0, "", true, 0, "")
-	pdf.CellFormat(40, 10, "33", "1", 0, "", true, 0, "")
-	pdf.CellFormat(40, 10, "55", "1", 0, "", true, 0, "")
-	pdf.CellFormat(40, 10, "60", "1", 0, "", true, 0, "")
+	for _, str := range headerText {
+		pdf.CellFormat(40, 10, str, "1", 0, "", true, 0, "")
+	}
 
 	pdf.Ln(-1)
+
+	return pdf
+}
+
+func table(pdf *gofpdf.Fpdf) *gofpdf.Fpdf {
 
 	pdf.SetFont("Times", "", 10)
 	pdf.SetFillColor(255, 255, 255)
@@ -43,12 +71,5 @@ func main() {
 
 	pdf.Ln(-1)
 
-	if pdf.Err() {
-		log.Fatalf("failed! %s", pdf.Error())
-	}
-
-	err := pdf.OutputFileAndClose("report-example.pdf")
-	if err != nil {
-		log.Fatalf("error saving pdf file: %s", err)
-	}
+	return pdf
 }
